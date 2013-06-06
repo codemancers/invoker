017969e8013f1c2a12fa7e57bc8810d8d032e9e3
Hemant Kumar <gethemant@gmail.com>
2013-06-06 12:27:45 +0530

    Setup Minitest and mocha

--- test/necro/test_commander.rb
+++ test/necro/test_commander.rb
@@ -1,7 +1,9 @@
-require "minitest/autorun"
+require "test_helper"
 
 class TestCommander < Minitest::Unit::TestCase
   def test_add_command_on_initialize
+
+    @commander.expects(:start).returns(true)
     assert_equal "foo", "foo"
   end
 end
---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+require "mocha/setup"
+
+__LIB_PATH__ = File.join(File.dirname(__FILE__), "..")
+$: << __LIB_PATH__
+
+require "necro"
+
+
+
+
