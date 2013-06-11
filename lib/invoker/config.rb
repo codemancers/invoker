fd9dadb1779751441c70c8e6f66b0744192089c5
Hemant Kumar <gethemant@gmail.com>
2013-06-11 10:35:49 +0530

    Add specs for checking directory of commands specified in config

--- lib/invoker/config.rb
+++ lib/invoker/config.rb
@@ -9,11 +9,20 @@ module Invoker
       @processes = process_ini(@ini_content)
     end
 
+    private
     def process_ini(ini_content)
       document = IniParse.parse(ini_content)
       document.map do |section|
+        check_directory(section["directory"])
         OpenStruct.new(label: section.key, dir: section["directory"], cmd: section["command"])
       end
     end
+
+    def check_directory(app_dir)
+      if app_dir && !app_dir.empty? && !File.directory?(app_dir)
+        raise Invoker::Errors::InvalidConfig.new("Invalid directory #{app_dir}")
+      end
+    end
+
   end
 end
---
+++ spec/invoker/config_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+require "tempfile"
+
+describe "Invoker::Config" do
+  describe "with invalid directory" do
+    it "should raise error during startup" do
+      begin
+        file = Tempfile.new("invalid_config.ini")
+
+        config_data =<<-EOD
+[try_sleep]
+directory = /Users/gnufied/foo
+command = ruby try_sleep.rb
+      EOD
+        file.write(config_data)
+        file.close
+        lambda {
+          Invoker::Config.new(file.path)
+        }.should.raise(Invoker::Errors::InvalidConfig)
+      ensure
+        file.unlink()
+      end
+    end
+  end
+end
+
