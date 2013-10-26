ec6c6f014eb6c8642e06bc05c28c0d2563380297
Amitava <amitava@codemancers.com>
2013-10-26 16:39:13 +0530

    Return error page if backend is not running

--- lib/invoker/power/balancer.rb
+++ lib/invoker/power/balancer.rb
@@ -99,11 +99,19 @@ module Invoker
       end
 
       def backend_data(backend, data)
-        data
+        @backend_data = data
       end
 
       def frontend_disconnect(backend, name)
         http_parser.reset()
+        unless @backend_data
+          Invoker::Logger.puts("\nBackend not running. Returning error page.".color(:red))
+          http_response = Invoker::Power::HttpResponse.new()
+          http_response.status = 503
+          http_response.use_file_as_body(File.join(File.dirname(__FILE__), "templates/error_page.html"))
+          connection.send_data(http_response.http_string)
+        end
+        @backend_data = nil
         connection.close_connection_after_writing() if backend == session
       end
 
