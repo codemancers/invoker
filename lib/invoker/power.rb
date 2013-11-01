5ceecb7c5cd4355f102506f1375010089696d8d8
Hemant Kumar <gethemant@gmail.com>
2013-11-02 00:22:32 +0530

    Remove files

--- lib/invoker/power.rb
+++
@@ -1,7 +0,0 @@
-require "invoker/power/config"
-require "invoker/power/port_finder"
-require "invoker/power/setup"
-require "invoker/power/http_response"
-require "invoker/power/dns"
-require "invoker/power/balancer"
-require "invoker/power/powerup"
--- lib/invoker/version.rb
+++
@@ -1,3 +0,0 @@
-module Invoker
-  VERSION = "1.0.3"
-end
