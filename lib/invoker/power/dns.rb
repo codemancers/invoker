ab90af63ece9d9e8bef17831a90ccb6b3876fc17
Hemant Kumar <gethemant@gmail.com>
2014-03-07 11:10:45 -0500

    Use new dns classes for running dns server

--- lib/invoker/power/dns.rb
+++ lib/invoker/power/dns.rb
@@ -2,8 +2,7 @@ require "logger"
 
 module Invoker
   module Power
-    class DNS
-      IN = Resolv::DNS::Resource::IN
+    class DNS < RubyDNS::Server
       def self.server_ports
         [
           [:udp, '127.0.0.1', Invoker::CONFIG.dns_port],
@@ -11,22 +10,22 @@ module Invoker
         ]
       end
 
-      def self.run_dns
-        RubyDNS::run_server(:listen => server_ports) do
-          on(:start) do
-            @logger.level = ::Logger::WARN
-          end
-
-          # For this exact address record, return an IP address
-          match(/.*\.dev/, IN::A) do |transaction|
-            transaction.respond!("127.0.0.1")
-          end
-
-          # Default DNS handler
-          otherwise do |transaction|
-            transaction.failure!(:NXDomain)
-          end
+      def process(name, resource_class, transaction)
+        if name_matches?(name) && resource_class_matches?(resource_class)
+          transaction.respond!("127.0.0.1")
+        else
+          transaction.fail!(:NXDomain)
         end
+      end
+
+      private
+
+      def resource_class_matches?(resource_class)
+        resource_class == Resolv::DNS::Resource::IN::A
+      end
+
+      def name_matches?(name)
+        name =~ /.*\.dev/
       end
     end
   end
