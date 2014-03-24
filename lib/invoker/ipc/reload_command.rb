cda34ca19fa4390b48cda3e64a13a0cba97b2469
Hemant Kumar <gethemant@gmail.com>
2014-03-23 20:25:31 -0400

    Fix invalid argument in reload command message

--- lib/invoker/ipc/reload_command.rb
+++ lib/invoker/ipc/reload_command.rb
@@ -1,7 +1,7 @@
 module Invoker
   module IPC
     class ReloadCommand < BaseCommand
-      def run_command
+      def run_command(message_object)
         Invoker::COMMANDER.on_next_tick(message_object) do |reload_message|
           reload_command(reload_message)
         end
