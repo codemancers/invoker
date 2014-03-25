080fa25459357ee12760b2c4c7b2b589156b7d6b
Hemant Kumar <gethemant@gmail.com>
2014-03-25 17:43:45 -0400

    Change http_process to process_name

--- lib/invoker/cli.rb
+++ lib/invoker/cli.rb
@@ -48,7 +48,7 @@ module Invoker
       unix_socket.send_command('add', process_name: name)
     end
 
-    desc "add_http http_process port", "Add an external http process to Invoker DNS server"
+    desc "add_http process_name port", "Add an external http process to Invoker DNS server"
     def add_http(name, port)
       unix_socket.send_command('add_http', process_name: name, port: port)
     end
