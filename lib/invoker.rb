4f88bff10f812e20da5dd65af109e8dd6d6802cb
Amitava <amitava@codemancers.com>
2014-04-06 01:45:00 +0530

    Cleanup Invoker module
    Follow the existing pattern of defining accessor methods.

--- lib/invoker.rb
+++ lib/invoker.rb
@@ -31,7 +31,9 @@ require "invoker/process_printer"
 module Invoker
   class << self
     attr_accessor :config, :tail_watchers, :commander
-    attr_accessor :dns_cache
+    attr_accessor :dns_cache, :daemonize
+
+    alias_method :daemonize?, :daemonize
 
     def darwin?
       ruby_platform.downcase.include?("darwin")
@@ -53,27 +55,20 @@ module Invoker
     rescue StandardError => error
       Invoker::Logger.puts "Error removing socket #{error}"
     end
-  end
 
-  def self.can_run_balancer?(throw_warning = true)
-    return false unless darwin?
-    return true if File.exist?(Invoker::Power::Config::CONFIG_LOCATION)
-
-    if throw_warning
-      Invoker::Logger.puts("Invoker has detected setup has not been run. Domain feature will not work without running setup command.".color(:red))
+    def daemon
+      @daemon ||= Invoker::Daemon.new
     end
-    false
+
+    def can_run_balancer?(throw_warning = true)
+      return false unless darwin?
+      return true if File.exist?(Invoker::Power::Config::CONFIG_LOCATION)
+
+      if throw_warning
+        Invoker::Logger.puts("Invoker has detected setup has not been run. Domain feature will not work without running setup command.".color(:red))
+      end
+      false
+    end
   end
 
-  def self.daemonize=(daemonize)
-    @daemonize = daemonize
-  end
-
-  def self.daemonize?
-    @daemonize
-  end
-
-  def self.daemon
-    @daemon ||= Invoker::Daemon.new
-  end
 end
