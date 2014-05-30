da5bd23f069048fb5c60b5d2b58e68417491e264
Hemant Kumar <gethemant@gmail.com>
2014-05-29 23:29:34 -0400

    Override restart_services method on redhat platform

--- lib/invoker/power/setup/distro/redhat.rb
+++ lib/invoker/power/setup/distro/redhat.rb
@@ -5,6 +5,12 @@ module Invoker
         def install_required_software
           system("yum --assumeyes install dnsmasq rinetd")
         end
+
+        def restart_services
+          system("systemctl enable rinetd")
+          system("service rinetd restart")
+          system("service dnsmasq restart")
+        end
       end
     end
   end
