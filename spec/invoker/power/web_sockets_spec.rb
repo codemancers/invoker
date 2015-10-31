efc3d2424bfdaa0514a8361c527580d2c05fe87c
Yuva <yuva@codemancers.com>
2015-11-01 00:23:39 +0530

    [websockets-support] use balancer in specs to tunnel websockets

--- spec/invoker/power/web_sockets_spec.rb
+++ spec/invoker/power/web_sockets_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
+# Full integration test. Start a server, and client. Let client interact with
+# server do a ping-pong. Client checks whether ping pong is successful or not.
+# Also, mock rewriter so that it returns valid port for request proxying.
+# - Server will run on port 28080.
+# - Balancer will run on port 28081 proxying to 28080
+# - Client will connect to 28081 performing ping-pong
+
 def websocket_server
   require 'websocket-eventmachine-server'
 
@@ -19,7 +26,7 @@ def websocket_client
   @message = ""
 
   EM.run do
-    ws = WebSocket::EventMachine::Client.connect(uri: 'ws://0.0.0.0:28080')
+    ws = WebSocket::EventMachine::Client.connect(uri: 'ws://0.0.0.0:28081')
     ws.onerror { |e| p e }
     ws.onopen { ws.send("ping") }
     ws.onmessage { |m, _| @message = m }
@@ -31,12 +38,24 @@ def websocket_client
   end
 end
 
-# Full integration test. Start a server, and client. Let client
-# interact with server do a ping-pong.
+
 describe 'Web sockets support' do
   it 'can ping pong via balancer' do
-    server = fork { websocket_server }
-    client = fork { websocket_client }
+    dns_response = Struct.new(:port).new(28080)
+    Invoker::Power::UrlRewriter.any_instance
+      .stubs(:select_backend_config)
+      .returns(dns_response)
+
+    EM.run do
+      EM.start_server("0.0.0.0", 28081, EM::ProxyServer::Connection, {}) do |conn|
+        Invoker::Power::Balancer.new(conn, "http").install_callbacks
+      end
+
+      fork { websocket_server }
+      fork { websocket_client }
+      EM.add_timer(3) { EM.stop }
+    end
+
     Process.waitall
   end
 end
