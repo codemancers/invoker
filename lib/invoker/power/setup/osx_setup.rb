7c778e5f7143ee2770f082ec98635e96b1ddd118
Nisanth Chunduru <nisanth074@gmail.com>
2016-01-20 08:50:25 +0530

    Fix: Resolver file wasn't being removed during uninstall if tld is custom

--- lib/invoker/power/setup/osx_setup.rb
+++ lib/invoker/power/setup/osx_setup.rb
@@ -22,6 +22,7 @@ module Invoker
         uninstall_invoker_flag = Invoker::CLI::Question.agree("Are you sure you want to uninstall firewall rules created by setup (y/n) : ")
 
         if uninstall_invoker_flag
+          Invoker::Power::Config.load_config
           remove_resolver_file
           unload_firewall_rule(true)
           Invoker::Power::Config.delete
