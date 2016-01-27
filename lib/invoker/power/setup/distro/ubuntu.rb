4ccc8e0d0dd3338883cbba4447c8aa0bfceb6132
Nisanth Chunduru <nisanth074@gmail.com>
2016-01-27 12:00:54 +0530

    Restart dnsmasq after creating resolver file in ubuntu

--- lib/invoker/power/setup/distro/ubuntu.rb
+++ lib/invoker/power/setup/distro/ubuntu.rb
@@ -4,6 +4,9 @@ module Invoker
   module Power
     module Distro
       class Ubuntu < Debian
+        def restart_services
+          system('service dnsmasq restart')
+        end
       end
     end
   end
