4bbdc6963d78ed80e556db8d9983418f58bf26a2
Nisanth Chunduru <nisanth074@gmail.com>
2016-02-14 23:39:35 +0530

    Remove redundant tld handling code

--- lib/invoker/power/power.rb
+++ lib/invoker/power/power.rb
@@ -1,29 +1,3 @@
 require "invoker/power/http_response"
 require "invoker/power/dns"
 require "invoker/power/balancer"
-
-module Invoker
-  module Power
-    class << self
-      def set_tld(tld_value)
-        @tld_value = tld_value
-      end
-
-      def reset_tld
-        @tld_value = nil
-      end
-
-      def tld
-        tld_value = @tld_value
-        if !tld_value && Invoker.config
-          tld_value = Invoker.config.tld
-        end
-        Tld.new(tld_value)
-      end
-
-      def tld_value
-        tld.value
-      end
-    end
-  end
-end
